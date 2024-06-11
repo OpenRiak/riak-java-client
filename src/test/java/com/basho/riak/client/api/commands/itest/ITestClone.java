e1c3785c16e13d52a60d3601dc7fbc4d0e7948e5
Caleb Prior <caleb.prior@users.noreply.github.com>
2024-06-11 17:39:28 +0100

    test triage - use different bucket for clone dest

--- src/test/java/com/basho/riak/client/api/commands/itest/ITestClone.java
+++ src/test/java/com/basho/riak/client/api/commands/itest/ITestClone.java
@@ -36,12 +36,11 @@ public class ITestClone extends ITestBase
         assertNotNull(fetchedBook);
         
 
-        Location cloneLocation = new Location(booksBucket, "moby_dick_cloned");
+        Location cloneLocation = new Location(new Namespace("cloned-books"), "moby_dick_cloned");
 
         CloneValue cloneBook = new CloneValue.Builder(bookLocations[0], cloneLocation).build();
 
         CloneValue.Response cloneResp = client.execute(cloneBook);
-        System.out.println(cloneResp);
 
         // Verify data was cloned
         FetchValue fetchClone = new FetchValue.Builder(cloneLocation).build();
