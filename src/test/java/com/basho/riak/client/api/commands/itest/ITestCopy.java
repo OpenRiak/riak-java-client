99d4e1a5964db3175a1d4e5fc794ba322a90ed4d
Caleb Prior <caleb.prior@users.noreply.github.com>
2024-09-09 12:35:45 +0100

    add more tests + test move

--- src/test/java/com/basho/riak/client/api/commands/itest/ITestCopy.java
+++ src/test/java/com/basho/riak/client/api/commands/itest/ITestCopy.java
@@ -9,27 +9,28 @@ import com.basho.riak.client.api.commands.kv.StoreValue;
 import com.basho.riak.client.core.operations.itest.ITestBase;
 import com.basho.riak.client.core.query.Location;
 import com.basho.riak.client.core.query.Namespace;
-import com.basho.riak.client.core.query.RiakObject;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.junit.Test;
 
 import java.util.concurrent.ExecutionException;
 
-import static junit.framework.TestCase.*;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
 
 public class ITestCopy extends ITestBase {
+
     private final RiakClient client = new RiakClient(cluster);
     private final Namespace booksBucket = new Namespace("books");
 
     @Test
     public void testCopySucceeds() throws ExecutionException, InterruptedException {
         // Insert Data
-        Location bookLocation = insertBookData(client);
+        Location bookLocation = new Location(booksBucket, "moby_dick");
+        insertBookData(client, bookLocation);
 
         // Verify Data was inserted
         FetchValue fetchMobyDickOp = new FetchValue.Builder(bookLocation).build();
         FetchValue.Response beforeCloneFetch = client.execute(fetchMobyDickOp);
-        System.out.println("Before Copy Fetched: " + beforeCloneFetch);
         Book fetchedBook = beforeCloneFetch.getValue(Book.class);
         assertNotNull(fetchedBook);
         assertEquals(fetchedBook.author, "Herman Melville");
@@ -37,28 +38,103 @@ public class ITestCopy extends ITestBase {
         Location copyLocation = new Location(new Namespace("copy-books"), "moby_dick");
         CopyValue copyBook = new CopyValue.Builder(bookLocation, copyLocation).build();
 
-        CopyValue.Response copyResp = client.execute(copyBook);
-        System.out.println("Copy resp: " + copyResp);
+        client.execute(copyBook);
 
         // Verify original still exists
         FetchValue.Response afterCopyFetch = client.execute(new FetchValue.Builder(bookLocation).build());
-        System.out.println("After copy fetch src: " + afterCopyFetch);
         fetchedBook = afterCopyFetch.getValue(Book.class);
         assertNotNull(fetchedBook);
+        assertEquals(fetchedBook.author, "Herman Melville");
 
         // Verify data was cloned
         FetchValue fetchClone = new FetchValue.Builder(copyLocation).build();
         FetchValue.Response cloneFetchRes = client.execute(fetchClone);
-        System.out.println("After clone fetch dest: " + cloneFetchRes);
-        assertNotNull(cloneFetchRes.getValue(RiakObject.class));
-
-        fail("TODO complete testing");
+        fetchedBook = cloneFetchRes.getValue(Book.class);
+        assertNotNull(fetchedBook);
+        assertEquals(fetchedBook.author, "Herman Melville");
     }
 
-    private Location insertBookData(RiakClient client)
-            throws ExecutionException, InterruptedException {
-        Location bookLocation = new Location(booksBucket, "moby_dick");
+    @Test
+    public void testCopySameBucket() throws ExecutionException, InterruptedException {
+        // Insert Data
+        Location bookLocation = new Location(booksBucket, "moby_dick_2");
+        insertBookData(client, bookLocation);
 
+        // Verify Data was inserted
+        FetchValue fetchMobyDickOp = new FetchValue.Builder(bookLocation).build();
+        FetchValue.Response beforeCloneFetch = client.execute(fetchMobyDickOp);
+        Book fetchedBook = beforeCloneFetch.getValue(Book.class);
+        assertNotNull(fetchedBook);
+        assertEquals(fetchedBook.author, "Herman Melville");
+
+        Location copyLocation = new Location(booksBucket, "moby_dick_copy");
+        CopyValue copyBook = new CopyValue.Builder(bookLocation, copyLocation).build();
+
+        client.execute(copyBook);
+
+        // Verify original still exists
+        FetchValue.Response afterCopyFetch = client.execute(new FetchValue.Builder(bookLocation).build());
+        fetchedBook = afterCopyFetch.getValue(Book.class);
+        assertNotNull(fetchedBook);
+        assertEquals(fetchedBook.author, "Herman Melville");
+
+        // Verify data was cloned
+        FetchValue fetchClone = new FetchValue.Builder(copyLocation).build();
+        FetchValue.Response cloneFetchRes = client.execute(fetchClone);
+        fetchedBook = cloneFetchRes.getValue(Book.class);
+        assertNotNull(fetchedBook);
+        assertEquals(fetchedBook.author, "Herman Melville");
+    }
+
+    @Test
+    public void testCopyNoSourceLocation() throws ExecutionException, InterruptedException {
+        Location sourceLocation = new Location(booksBucket, "unknown_source_location");
+        Location copyLocation = new Location(new Namespace("copy-books"), "unused_dest_location");
+
+        CopyValue copy = new CopyValue.Builder(sourceLocation, copyLocation).build();
+        client.execute(copy);
+    }
+
+    @Test
+    public void testCopyReturnBodyTrue() throws ExecutionException, InterruptedException {
+        // Insert Data
+        Location bookLocation = new Location(booksBucket, "moby_dick_3");
+        insertBookData(client, bookLocation);
+
+        // Verify Data was inserted
+        FetchValue fetchMobyDickOp = new FetchValue.Builder(bookLocation).build();
+        FetchValue.Response beforeCloneFetch = client.execute(fetchMobyDickOp);
+        Book fetchedBook = beforeCloneFetch.getValue(Book.class);
+        assertNotNull(fetchedBook);
+        assertEquals(fetchedBook.author, "Herman Melville");
+
+        Location copyLocation = new Location(new Namespace("copy-books"), "moby_dick_2");
+        CopyValue copyBook = new CopyValue.Builder(bookLocation, copyLocation)
+                .withOption(CopyValue.Option.RETURN_BODY, true)
+                .build();
+
+        // Returned the body in the response
+        CopyValue.Response response = client.execute(copyBook);
+        fetchedBook = response.getValue(Book.class);
+        assertNotNull(fetchedBook);
+        assertEquals(fetchedBook.author, "Herman Melville");
+
+        // Verify original still exists
+        FetchValue.Response afterCopyFetch = client.execute(new FetchValue.Builder(bookLocation).build());
+        fetchedBook = afterCopyFetch.getValue(Book.class);
+        assertNotNull(fetchedBook);
+        assertEquals(fetchedBook.author, "Herman Melville");
+
+        // Verify data was cloned
+        FetchValue fetchClone = new FetchValue.Builder(copyLocation).build();
+        FetchValue.Response cloneFetchRes = client.execute(fetchClone);
+        fetchedBook = cloneFetchRes.getValue(Book.class);
+        assertNotNull(fetchedBook);
+        assertEquals(fetchedBook.author, "Herman Melville");
+    }
+
+    private void insertBookData(RiakClient client, Location location)
+            throws ExecutionException, InterruptedException {
         Book mobyDick = new Book();
         mobyDick.title = "Moby Dick";
         mobyDick.author = "Herman Melville";
@@ -66,10 +142,8 @@ public class ITestCopy extends ITestBase {
         mobyDick.isbn = "1111979723";
         mobyDick.copiesOwned = 3;
 
-        StoreValue storeBookOp = new StoreValue.Builder(mobyDick).withLocation(bookLocation).build();
+        StoreValue storeBookOp = new StoreValue.Builder(mobyDick).withLocation(location).build();
         client.execute(storeBookOp);
-
-        return bookLocation;
     }
 
     public static class Book {
